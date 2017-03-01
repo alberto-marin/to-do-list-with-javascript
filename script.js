ac63abfb24002eadc15220ed46c326704ad28d29
Alberto Marin <hello@albertomar.in>
2017-03-01 10:39:53 +0000

    Add edit and display mode in css. Add funtionality to the events.

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded',function(){
+  // https://github.com/gionkunz/chartist-js/issues/85
+  var todolist = document.getElementById('todolist');
+
+  var elements = todolist.querySelectorAll('li');
+  var inputs = todolist.querySelectorAll('input');
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', editElement);
+    inputs[i].addEventListener('blur', updateElement);
+    inputs[i].addEventListener('keypress', elementKeyPress);
+  }
+
+  function editElement() {
+    this.className = 'edit';
+    var input = this.querySelector('input');
+    input.focus();
+    input.setSelectionRange(0, input.value.length);
+  }
+  /* Update the element only when Enter is press */
+  function updateElement(event) {
+    this.previousElementSibling.innerHTML = this.value;
+    this.parentNode.className = '';
+  }
+
+  function elementKeyPress(event) {
+    if (event.which === 13) {
+      updateElement.call(this);
+    }
+  }
+
+
+});
